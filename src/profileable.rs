fc713901be00fc2e3c4a5160d4c26500271fd806
AdoKa3710 <adicoolwest@gmail.com>
2021-11-26 17:08:31 +0600

    Fix tag profileable

--- src/profileable.rs
+++ src/profileable.rs
@@ -1,16 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-/// Specifies how the following tools can access profiling information:
-///
-/// * [`android.os.Trace`] tracing APIs
-/// * [`simpleperf`]
-/// * [`am profile`] commands
-///
-/// Unless this element is declared, and the [`<shell>`] tag set to true,
-/// the above tools and APIs will work only when an app is [`debuggable`].
-/// Debuggable apps incur significant and varied performance degradation,
-/// and are not useful for measuring timing accurately.This element is strongly
-/// recommended for local performance measurements, in order to capture accurate results.
+/// Specifies how profilers can access this application.
 ///
 /// ## XML Syntax
 /// ```xml
@@ -31,7 +21,31 @@ use serde::{Deserialize, Serialize};
 /// [`debuggable`]: https://developer.android.com/guide/topics/manifest/application-element#debug
 #[derive(Debug, Deserialize, Serialize, YaSerialize, YaDeserialize, PartialEq, Default, Clone)]
 pub struct Profileable {
-    /// Indicates whether this application may be profiled by the shell process.
+    /// Specifies whether the user of the device can profile this application 
+    /// through local debugging tools. These include
+    /// 
+    /// * [`android.os.Trace`] tracing APIs (Android 11 and lower)
+    /// * [`simpleperf`]
+    /// * [`am profile commands`]
+    /// * [`perfetto profilers`] (native memory, Java memory, CPU).
+    /// 
+    /// If this isn't set or is set to false, these tools and APIs will work
+    /// only when an app is debuggable. Debuggable apps incur significant and
+    /// varied performance degradation, and are not useful for measuring timing
+    /// accurately. This element is strongly recommended for local performance 
+    /// measurements, in order to capture accurate results.
+    /// 
+    /// [`android.os.Trace`]: https://developer.android.com/reference/kotlin/android/os/Trace
+    /// [`simpleperf`]: https://developer.android.com/ndk/guides/simpleperf
+    /// [`am profile commands`]: 
     #[yaserde(attribute, prefix = "android")]
     pub shell: bool,
+    /// Specifies whether the application can be profiled by system services or
+    /// shell tools (for the latter, you must also set [`android:shell`]). If 
+    /// false, the application cannot be profiled at all. Defaults to true. This 
+    /// attribute was added in API level 30.
+    /// 
+    /// [`android:shell`]: https://developer.android.com/guide/topics/manifest/profileable-element#shell
+    #[yaserde(attribute, prefix = "android")]
+    pub enable: bool,
 }
