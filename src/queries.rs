1f7a593ad7f98f7672e0cee387c85cb14a8309ea
unknown <den_9912@mail.ru>
2022-06-30 19:48:28 +0500

    Fix queries provider field

--- src/queries.rs
+++ src/queries.rs
@@ -1,5 +1,6 @@
+use crate::Provider;
+
 use super::action::Action;
-use super::attribute_list::{AttributeList, Semicolon};
 use super::data::Data;
 use serde::{Deserialize, Serialize};
 
@@ -55,7 +56,7 @@ pub struct Queries {
     ///
     /// [`<provider>`]: crate::Provider
     /// [`content provider authorities`]: https://developer.android.com/guide/topics/providers/content-provider-basics#ContentURIs
-    pub provider: Vec<QueriesProvider>,
+    pub provider: Vec<Provider>,
 }
 
 /// Specifies a single app that your app intends to access. This other app might integrate
@@ -83,91 +84,4 @@ pub struct Package {
 pub struct Intent {
     pub action: Action,
     pub data: Vec<Data>,
-}
-
-/// Declares a content provider component used in [`<queries>`].
-///
-/// A content provider is a subclass of [`ContentProvider`] that supplies structured
-/// access to data managed by the application. All content providers in your application
-/// must be defined in a `<provider>` element in the manifest file; otherwise, the system
-/// is unaware of them and doesn't run them.
-///
-/// You only declare content providers that are part of your application. Content
-/// providers in other applications that you use in your application should not be
-/// declared.
-///
-/// The Android system stores references to content providers according to an authority
-/// string, part of the provider's content URI. For example, suppose you want to access a
-/// content provider that stores information about health care professionals.
-/// To do this, you call the method [`ContentResolver.query()`],
-/// which among other arguments takes a URI that identifies the provider:
-///
-/// ## XML Example
-/// ```xml
-/// content://com.example.project.healthcareprovider/nurses/rn
-/// ```
-///
-/// The `content:` `scheme` identifies the URI as a content URI pointing to an Android
-/// content provider. The authority `com.example.project.healthcareprovider` identifies
-/// the provider itself; the Android system looks up the authority in its list of known
-/// providers and their authorities. The substring `nurses/rn` is a `path`, which the
-/// content provider can use to identify subsets of the provider data.
-///
-/// In cases where you need to query a content provider but don't know the specific
-/// package names, you can declare that provider authority in a <provider> element,
-/// as shown in the following snippet:
-///
-/// ## XML Example
-/// ```xml
-/// <manifest package="com.example.suite.enterprise">
-///     <queries>
-///         <provider android:authorities="com.example.settings.files" />
-///     </queries>
-///    ...
-/// </manifest>
-/// ```
-///
-/// ## Node
-/// If your <queries> element includes a <provider> element, you might see an editor
-/// warning in Android Studio related to the <provider> element. As long as you're using
-/// the latest "dot" release of the Android Gradle plugin, your build is unaffected, so
-/// you can disregard the warning. Learn more in the blog post about [`Preparing your
-/// Gradle build for package visibility in Android 11`].
-///
-/// ## Contained in
-/// * [`<queries>`]
-///
-/// ## Introduced in
-/// API Level 30
-///
-/// [`ContentProvider`]: https://developer.android.com/reference/android/content/ContentProvider
-/// [`ContentResolver.query()`]: https://developer.android.com/reference/android/content/ContentResolver#query(android.net.Uri,%20java.lang.String[],%20android.os.Bundle,%20android.os.CancellationSignal)
-/// [`Content Providers`]: https://developer.android.com/guide/topics/providers/content-providers
-/// [`<queries>`]: crate::Queries
-/// [`Preparing your Gradle build for package visibility in Android 11`]: https://android-developers.googleblog.com/2020/07/preparing-your-build-for-package-visibility-in-android-11.html
-#[derive(Debug, Deserialize, Serialize, YaSerialize, YaDeserialize, PartialEq, Default, Clone)]
-#[serde(rename = "provider")]
-pub struct QueriesProvider {
-    /// A list of one or more URI authorities that identify data offered by the content
-    /// provider. Multiple authorities are listed by separating their names with a
-    /// semicolon. To avoid conflicts, authority names should use a Java-style naming
-    /// convention (such as com.example.provider.cartoonprovider). Typically, it's the
-    /// name of the [`ContentProvider`] subclass that implements the provider
-    ///
-    /// There is no default. At least one authority must be specified.
-    ///
-    /// [`ContentProvider`]: https://developer.android.com/reference/android/content/ContentProvider
-    #[yaserde(
-        attribute,
-        prefix = "android",
-        skip_serializing_if = "check_authorities"
-    )]
-    #[serde(skip_serializing_if = "AttributeList::is_empty")]
-    pub authorities: AttributeList<Semicolon, String>,
-}
-
-impl QueriesProvider {
-    pub fn check_authorities(&self, value: &AttributeList<Semicolon, String>) -> bool {
-        value.is_empty()
-    }
 }
